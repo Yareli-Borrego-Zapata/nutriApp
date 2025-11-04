4f647a12561e31ecef03fbb29417e1c672d3c72e
Yareli-Borrego-Zapata <23308060610278@cetis61.edu.mx>
2025-11-03 17:32:17 -0700

    Actualizacion

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template , request, redirect, url_for, flash, session
+from datetime import datetime 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/base")
+def menu():
+    return render_template("base.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
