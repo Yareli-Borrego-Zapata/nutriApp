049173d9b705ea02c08b530a5190c4a95a588085
EsmeraldaMTZV <23308060610235@cetis61.edu.mx>
2025-11-24 09:12:22 -0600

    act

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template , request, redirect, url_for, flash, session
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash
 from datetime import datetime 
 
 app = Flask(__name__)
 app.secret_key = "Nutriflow1234"
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'prueba'
+
+
 
 usuarios = [
     {
